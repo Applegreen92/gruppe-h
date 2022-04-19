2f1be1d2fbc6a357c8a89a675e5e9207abf3bce0
David <david.rumpf@stud.uni-due.de>
2022-04-19 17:43:58 +0200

    Completed Movie class variables

--- src/main/java/com/model/Movie.java
+++ src/main/java/com/model/Movie.java
@@ -4,11 +4,16 @@ import java.util.Date;
 
 public class Movie {
 
-    public Movie(String title, String genre,String posterSrc, int date){
+    public Movie(String title, String genre,String posterSrc, int date, int length,String regisseur,String author,String cast){
         this.title = title;
         this.posterSrc = posterSrc;
         this.genre = genre;
         this.releaseDate = date;
+        this.length = length;
+        this.regisseur = regisseur;
+        this.author = author;
+        this.cast = cast;
+
      }
 
 
@@ -16,26 +21,26 @@ public class Movie {
     private String posterSrc;
     private String genre;
     private int releaseDate;
+    private int length;
+    private String regisseur;
+    private String author;
+    private String cast;
+
 
     public String getTitle(){
         return title;
     }
-
     public String getGenre(){
         return genre;
     }
-
     public String getPosterSrc(){
         return posterSrc;
     }
-
     public int getReleaseDate(){
         return releaseDate;
     }
-
-
-
-
-
-
+    public int getLength() { return length; }
+    public String getRegisseur() {return regisseur;}
+    public String getAuthor() {return author;}
+    public String getCast() {return cast;}
 }
