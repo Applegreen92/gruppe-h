cfaeddf856946a4d6d10cce0814905e31ba2f2d5
David <david.rumpf@stud.uni-due.de>
2022-04-20 10:36:43 +0200

    AddMovieManually class

---
+++ src/main/java/com/controller/addMovieManually.java
@@ -0,0 +1,29 @@
+package com.controller;
+
+import com.model.Movie;
+
+import java.util.Scanner;
+
+public class addMovieManually {
+    Scanner scanner = new Scanner(System.in);
+
+    private String title = scanner.next();
+
+    private String posterSrc = scanner.next();
+
+    private String genre = scanner.next();
+
+    private int releaseDate = scanner.nextInt();
+
+    private int length = scanner.nextInt();
+
+    private String regisseur = scanner.next();
+
+    private String author = scanner.next();
+    
+    private String cast = scanner.next();
+
+    public Movie addMovieButton(){
+        return new Movie(title,posterSrc,genre,releaseDate,length,regisseur,author,cast);
+    }
+}
