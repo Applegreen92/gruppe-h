dda3323634de11716e3c2ad329774f052f46579a
Tobias <tobias.zettelmeyer@stud.uni-due.de>
2022-04-20 18:18:12 +0200

    crawler wip
    crawler retrieves data, but its not the right one :/

--- src/main/java/com/model/Crawler.java
+++ src/main/java/com/model/Crawler.java
@@ -16,21 +16,25 @@ public class Crawler {
 
         try {
             //Navigates to the IMDB website based on the passed genre and copy's html-code into the document 'movies'
-            Document movies = Jsoup.connect("https://www.imdb.com/search/title/?genres=" + genre +
-                    "&title_type=tv_series,mini_series&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=b4e1d6fb-9821-4c7d-ad14-31ed10854442&pf_rd_r=24E9EQTTJXMRQDBDQDVJ&pf_rd_s=center-7&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_tvpop_1").get();
+            Document movies = Jsoup.connect("https://www.imdb.com/search/title/?genres="+genre+"&title_type=feature&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=facfbd0c-6f3d-4c05-9348-22eebd58852e&pf_rd_r=EJ5YBM5AY176J6Y11SY7&pf_rd_s=center-6&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_mvpop_1").get();
             Elements body = movies.select("div.lister-list");
+            //System.out.println(body);
 
             if(!body.isEmpty()) {
 
                 //lister.item is the list with the movie entries.
-                for (Element movie : body.select("lister.item")) {
+                for (Element movie : body.select("div, .lister.item")) {
 
                     //scans for the movie poster and saves them as a link. They are found under lister-item-image and
                     // marked with a 'src' tag ,which specifies the location of an external resource.
-                    String img = movie.select("lister-item-image img").attr("src");
+
+
+                    String img = movie.select("img, .loadlate").attr("src");
                     //System.out.println(img);
 
-                    //String title = movie.select("lister-item-header").attr("src");
+                    String title = movie.select("a, lister-item-header").text().strip();
+                    System.out.println(title);
+
 
 
                 }
