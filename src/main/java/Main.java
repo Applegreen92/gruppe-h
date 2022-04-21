167b364f5f71b4ef09ce6d0baa3bfa0288d3a334
Tobias <tobias.zettelmeyer@stud.uni-due.de>
2022-04-21 21:27:00 +0200

    No visible progress
    I failed miserably

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Crawler.getMovies("action");
+       Crawler insertMovies = new Crawler();
+       insertMovies.getMovies("action");
     }
 }
--- src/main/java/com/model/Crawler.java
+++ src/main/java/com/model/Crawler.java
@@ -7,33 +7,36 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.lang.String;
 
 import java.io.IOException;
 
 public class Crawler {
 
-    public static void getMovies(String genre) throws IOException{
+    public void getMovies(String genre) throws IOException{
 
         try {
             //Navigates to the IMDB website based on the passed genre and copy's html-code into the document 'movies'
-            Document movies = Jsoup.connect("https://www.imdb.com/search/title/?genres="+genre+"&title_type=feature&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=facfbd0c-6f3d-4c05-9348-22eebd58852e&pf_rd_r=EJ5YBM5AY176J6Y11SY7&pf_rd_s=center-6&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_mvpop_1").get();
+            Document movies = Jsoup.connect("https://www.imdb.com/search/title/?genres="+ genre +"&title_type=feature&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=facfbd0c-6f3d-4c05-9348-22eebd58852e&pf_rd_r=EJ5YBM5AY176J6Y11SY7&pf_rd_s=center-6&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_mvpop_1").get();
             Elements body = movies.select("div.lister-list");
             //System.out.println(body);
 
             if(!body.isEmpty()) {
 
                 //lister.item is the list with the movie entries.
-                for (Element movie : body.select("div, lister.item mode advanced")) {
+                for (Element movie : body.select("div, lister.item")) {
 
                     //scans for the movie poster and saves them as a link. They are found under lister-item-image and
                     // marked with a 'src' tag ,which specifies the location of an external resource.f
                     //WORKING!!!
                     String img = movie.select("img, .loadlate").attr("loadlate");
-                    //System.out.println(img);
 
-                    String title = movie.select("a, [href*=title]").text();
 
-                    String title2 = movie.select("h3, p:first-child").text();
+                    String title2 = movie.select("h3, h3:nth-child(2)").text();
+
+
+
+                    String genreLengthAge = movie.select("h3, h3:first-child").next().text();
 
                     System.out.println(title2);
 
