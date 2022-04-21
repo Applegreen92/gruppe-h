d226f1521760492d5685a2cb56db3544be08c509
David <david.rumpf@stud.uni-due.de>
2022-04-21 13:38:37 +0200

    added User class

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -2,8 +2,25 @@ package com.model;
 
 public class User {
 
-    User (){
-
+    public User (String givenName, String familyName, String eMail, String password,boolean isSystemAdmin){
+    this.givenName = givenName;
+    this.familyName = familyName;
+    this.eMail = eMail;
+    this.password = password;
+    this.isSystemAdmin = isSystemAdmin;
     }
+
+    private String givenName;
+    private String familyName;
+    private String eMail;
+    private String password;
+    private boolean isSystemAdmin;
+
+    public String getGivenName() { return givenName; }
+    public String getFamilyName() { return familyName; }
+    public String geteMail() { return eMail; }
+    public String getPassword() { return password; }
+    public boolean isSystemAdmin() { return isSystemAdmin; }
+
 
 }
