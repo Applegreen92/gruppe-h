b9ff66c7c78f33df277c7575d7378f6a5080762a
David <david.rumpf@stud.uni-due.de>
2022-04-23 10:04:50 +0200

    MyGUI

---
+++ src/main/java/com/controller/addMovieManually.java
@@ -0,0 +1,23 @@
+package com.controller;
+
+import com.example.gruppeh.HelloApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class addMovieManually extends Application {
+
+
+
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("addFilmGUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+    public static void main(String[] args) {launch(args);}
+}
