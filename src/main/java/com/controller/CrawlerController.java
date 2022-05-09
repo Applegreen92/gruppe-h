0529bddc39c9a9f19f7998f4917229c6a8959c44
Saiyan <psaiyan@yahoo.de>
2022-05-09 16:31:33 +0200

    crawlercontroller

--- src/main/java/com/controller/CrawlerController.java
+++ src/main/java/com/controller/CrawlerController.java
@@ -1,11 +1,13 @@
 package com.controller;
 
+import com.model.Crawler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,9 +17,11 @@ public class CrawlerController extends SceneController implements Initializable 
     @FXML
     public TextField genres, startyear, endyear;
 
-    public void startNow(ActionEvent actionEvent) {
+    public void startNow(ActionEvent actionEvent) throws IOException {
+        Crawler autodownload = new Crawler();
 
-        
+        autodownload.getMoviesByGenre(genres.getText(),1,Integer.parseInt(startyear.getText()),Integer.parseInt(endyear.getText()));
+
 
 
     }
