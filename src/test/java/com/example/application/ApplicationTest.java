da1ab89654ddc33c2ecce32a3c5a2bc04ba94ec9
Felix Bernardi <Felix.Bernardi@stud.uni-due.de>
2022-07-02 10:26:11 +0200

    Added Test Structure

---
+++ src/test/java/com/example/application/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.application;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    public void contextLoads(){
+
+    }
+
+}
